9c163382e157c7cdd1cc1d067f3c58405a7c8bb7
alelmb <burlamaquialexandre@gmail.com>
2024-06-01 20:59:38 -0300

    Main Menu pronto

--- scenes/menu/main_menu/main_menu.gd
+++ scenes/menu/main_menu/main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var frontscreenTexture = $FrontscreenTexture
+@onready var mainPanel = $Panel
+@onready var returnButton = $Return
+@onready var VboxCredits = $VBOXCredits
 func _on_play_pressed():
 	get_tree().change_scene_to_file('res://scenes/stages/home.tscn')
 
@@ -9,5 +13,17 @@ func _on_options_pressed():
 
 
 func _on_credits_pressed():
+	frontscreenTexture.visible = not is_visible_in_tree()
+	mainPanel.visible = not is_visible_in_tree()
+	returnButton.visible = is_visible_in_tree()
+	VboxCredits.visible = is_visible_in_tree()
 	print('Credits successful!')
 
+
+
+
+func _on_return_pressed():
+	frontscreenTexture.visible = is_visible_in_tree()
+	mainPanel.visible = is_visible_in_tree()
+	returnButton.visible = not is_visible_in_tree()
+	VboxCredits.visible = not is_visible_in_tree()
