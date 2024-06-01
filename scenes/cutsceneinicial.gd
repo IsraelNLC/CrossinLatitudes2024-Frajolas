aecc89a2a47f1de9cf5efc8eb3109bc456be64aa
gui23g <guilherme.carvalho@sou.inteli.edu.br>
2024-06-01 17:48:20 -0300

    Revert "Merge remote-tracking branch 'origin/Dialog_UI' into feat-emil_sprites"
    This reverts commit 672acb4e63b4904e6c050cdc9f73610f27286075, reversing
    changes made to cbe9da51aa282bd862a794c5be8d44327cf76396.

--- scenes/cutsceneinicial.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var colorrect: ColorRect
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	DialogueManager.show_example_dialogue_balloon(load("res://introcutscene.dialogue"), "cutscenestart")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- scenes/scene_manager.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-# Called when the node enters the scene tree for the first time.
-func _process(delta):
-	if Input.is_action_just_pressed("interact1"):
-		$TransitionScreen.transition()
