1081861d2b1ac7887f6bd35e68cd637a9aed88fb
alelmb <burlamaquialexandre@gmail.com>
2024-06-01 21:18:00 -0300

    Dialog UI + Cutscene inicial finalizados

--- scenes/cutsceneinicial.gd
+++ scenes/cutsceneinicial.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 var colorrect: ColorRect
+@onready var globalvars = $"/root/Globalvars"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	DialogueManager.show_example_dialogue_balloon(load("res://introcutscene.dialogue"), "cutscenestart")
+	return
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (globalvars.isdialogueover == true):
+		get_tree().change_scene_to_file("res://scenes/stages/home.tscn")
+	
