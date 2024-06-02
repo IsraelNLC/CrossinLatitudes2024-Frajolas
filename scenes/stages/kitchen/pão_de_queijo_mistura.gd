96b4630145f49e6739bc1094bf272c740faf500a
Israel <israel.carvalho@sou.inteli.edu.br>
2024-06-02 12:19:46 -0300

    fea. legal

--- scenes/stages/kitchen/pão_de_queijo_mistura.gd
+++ scenes/stages/kitchen/pão_de_queijo_mistura.gd
@@ -1,41 +1,67 @@
 extends Node2D
 
+
 @onready var tigela1 = $hand_npc
 @onready var tigela2 = $hand_player
 
 @onready var main = $"../"
+
+@onready var timer = $timerAnim
+@onready var timer2 = $timerAnim2
+
+#var nextStage1 = 1
+#var nextStage2 = 1
 
 func start_animation():
 	main.enrolar.visible = false
 	main.ralador.visible = false
 	main.misturar.visible = true
 
-func mix_1():
-	if tigela1.position.x == 291:
-		if tigela1.position.y == 521:
-			tigela1.position.x = 354
-			return
-		else:
-			tigela1.position.y = 521
-			return
-	elif tigela1.position.x ==354 && tigela1.position.y == 521:
-		tigela1.position.y = 547
-		return
-	else:
-			tigela1.position.y = 521
-			return
-			
-func mix_2():
-	if tigela2.position.x == 931:
-		if tigela2.position.y == 521:
-			tigela2.position.x = 994
-			return
-		else:
-			tigela2.position.y = 931
-			return
-	elif tigela2.position.x == 994 && tigela2.position.y == 521:
-		tigela2.position.y = 547
-		return
-	else:
-			tigela2.position.y = 931
-			return
+func mix_1(stage1):
+	timer.wait_time = 0.5
+	var pos11 = Vector2(291,521)
+	var pos12 = Vector2(354,521)
+	var pos13 = Vector2(354,547)
+	
+	if stage1 == 1:
+		timer.start()
+		await timer.timeout
+		tigela1.position = pos11
+		return 2
+	if stage1 == 2:
+		timer.start()
+		await timer.timeout
+		tigela1.position = pos12
+		return 3
+	if stage1 == 3:
+		timer.start()
+		await timer.timeout
+		tigela1.position = pos13
+		return 1
+
+
+func mix_2(stage2):
+	timer2.wait_time = 0.5
+	var pos21 = Vector2(931,521)
+	var pos22 = Vector2(994,521)
+	var pos23 = Vector2(994,547)
+	
+	if stage2 == 1:
+		timer2.start()
+		await timer2.timeout
+		tigela2.position = pos21
+		return 2
+	if stage2 == 2:
+		timer2.start()
+		await timer2.timeout
+		tigela2.position = pos22
+		return 3
+	if stage2 == 3:
+		timer2.start()
+		await timer2.timeout
+		tigela2.position = pos23
+		return 1
+
+
+
+
--- scenes/stages/pão_de_queijo.gd
+++ scenes/stages/pão_de_queijo.gd
@@ -4,31 +4,47 @@ extends Node2D
 @onready var enrolar = $Cooking3
 @onready var misturar = $Cooking
 
-var current_beat = 0
+var mixStage1 = 1
+var mixStage2 = 1
+
+#var current_beat = 0
 
 #var cenas = [ralador.start_animation(),ralador.play_1(),ralador.play_1(),ralador.play_2(),ralador.play_2(),ralador.play_1(),ralador.play_1(),ralador.play_2(),ralador.play_2(),misturar.start_animation(),misturar.mix_1(),misturar.mix_1(),misturar.mix_1(),misturar.mix_2(),misturar.mix_2(),misturar.mix_2(),ralador.start_animation(),ralador.play_1(),ralador.play_1(),ralador.play_2(),ralador.play_2(),misturar.start_animation(),misturar.mix_1(),misturar.mix_1(),misturar.mix_1(),misturar.mix_2(),misturar.mix_2(),misturar.mix_2(),enrolar.start_animation(),enrolar.enrola_1(),enrolar.enrola_1(),enrolar.stop_animation1(),enrolar.enrola_2(),enrolar.enrola_2(),enrolar.stop_animation2(),ralador.start_animation(),ralador.play_1(),ralador.play_1(),ralador.play_2(),ralador.play_2(),misturar.start_animation(),misturar.mix_1(),misturar.mix_1(),misturar.mix_1(),misturar.mix_2(),misturar.mix_2(),misturar.mix_2(),enrolar.start_animation(),enrolar.enrola_1(),enrolar.stop_animation1(),enrolar.enrola_2(),enrolar.stop_animation2()]
-var cenas = [0,1,1,2,2,3,4,5,4,5]
-
+var cenas = [0,1,1,2,2,1,1,2,2,3,4,4,4,5,5,5,0,1,1,2,2,3,4,4,4,5,5,5,6,7,8,9,10,7,8,9,10,0,1,1,2,2,3,4,4,4,5,5,5,6,7,8,9,10]
+#var cenas = [3,4,4,4,5,5,5,0,1,1,2,2,3,4,4,4,5,5,5,6,7,8,9,10,7,8,9,10,0,1,1,2,2,3,4,4,4,5,5,5,6,7,8,9,10]
+#enrolar.start_animation(),enrolar.enrola_1(),enrolar.enrola_1(),enrolar.stop_animation1(),enrolar.enrola_2()
 func _ready():
 	start_sequence()
-	#ralador.play_2()
 
 func start_sequence():
-	#for current_beat in cenas:
-	if cenas[current_beat] == 0:
-		ralador.start_animation()
-	elif cenas[current_beat] == 1:
-		print("animacao1")
-		if await ralador.play_1() == 1:
-			print("finnished")
-	elif cenas[current_beat] == 2:
-		print("animacao2")
-		if await ralador.play_2() == 2:
-			print("finnished")
-	elif cenas[current_beat] == 3:
-		misturar.start_animation()
-	elif cenas[current_beat] == 4:
-		misturar.mix_1()
-	elif cenas[current_beat] == 5:
-		misturar.mix_2()
-	current_beat += 1
+	for current_beat in cenas:
+		print(current_beat)
+		if current_beat == 0:
+			ralador.start_animation()
+		elif current_beat == 1:
+			print("animacao1")
+			if await ralador.play_1() == 1:
+				print("finnished")
+		elif current_beat == 2:
+			print("animacao2")
+			if await ralador.play_2() == 2:
+				print("finnished")
+		elif current_beat == 3:
+			misturar.start_animation()
+		elif current_beat == 4:
+			mixStage1 = await misturar.mix_1(mixStage1)
+			print("finnishedmix1")
+		elif current_beat == 5:
+			mixStage2 = await misturar.mix_2(mixStage2)
+			print("finnishedmix2")
+		elif current_beat == 6:
+			enrolar.start_animation()
+		elif current_beat == 7:
+			enrolar.enrola_1()
+		elif current_beat == 8:
+			enrolar.stop_animation1()
+		elif current_beat == 9:
+			enrolar.enrola_2()
+		elif current_beat == 10:
+			enrolar.stop_animation2()
+	#current_beat += 1
